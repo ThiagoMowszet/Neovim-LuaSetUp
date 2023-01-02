cec8a22fe0c467c9ca90d689e9825889422bf897
Thiago <thiagomowszet@gmail.com>
2023-01-02 20:30:30 -0300

    resolved conflic errors

--- lua/thiago/packer.lua
+++ lua/thiago/packer.lua
@@ -2,7 +2,6 @@
 
 -- Only required if you have packer configured as `opt`
 
-
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
--- lua/thiago/remap.lua
+++ lua/thiago/remap.lua
@@ -1,4 +1,4 @@
-im.g.mapleader = " "
+vim.g.mapleader = " "
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
