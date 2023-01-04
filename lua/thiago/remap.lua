6b70f3171b62ceea7d6d9b525bdadc518e43f0cd
Thiago <thiagomowszet@gmail.com>
2023-01-04 00:24:48 -0300

    add tabs/windows manager

--- lua/thiago/remap.lua
+++ lua/thiago/remap.lua
@@ -20,3 +20,9 @@ vim.keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
 vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 vim.keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
 vim.keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+
+vim.keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+vim.keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+vim.keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+vim.keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
