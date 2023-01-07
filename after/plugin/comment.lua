209b9e1b66879944a144b09afcbcc6f0cd75049f
Thiago <thiagomowszet@gmail.com>
2023-01-07 15:30:34 -0300

    minor changes

--- after/plugin/comment.lua
+++ after/plugin/comment.lua
@@ -6,3 +6,5 @@ end
 
 -- enable comment
 comment.setup() -- keymap for fast comment it's with : gcc | More than 1 line it's with : gc{lines} k or j, example gc10j <- comments 10 lines down
+
+-- also, with vim surround we can surround a word with the keymap : ysw"", to undo this, we can do: ds"", or if i want to change "" to '', we can do cs""''
