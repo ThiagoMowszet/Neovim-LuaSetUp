c01dd50139d2db3c95f18f039734f49f11495077
Thiago <thiagomowszet@gmail.com>
2023-01-09 18:29:01 -0300

    change arrows color nvim-tree

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -9,7 +9,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- change color for arrows in tree to light blue
-vim.cmd([[ highlight NvimTreeIndentMarker guifg=#B17634]])
+vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3A56E1]])
 
 -- configure nvim-tree
 nvimtree.setup({
