58e462fb07a2027de3bb61260b010ab49f0a31b1
Thiago <thiagomowszet@gmail.com>
2024-06-22 23:16:11 -0300

    change git signs deprecated values

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-	color = color or "tokyonight-storm"
+	color = color or "tokyonight-night"
 	vim.cmd.colorscheme(color)
 end
 
--- after/plugin/comment.lua
+++ after/plugin/comment.lua
@@ -5,7 +5,7 @@ if not setup then
 end
 
 -- enable comment
-comment.setup() 
+comment.setup()
 -- keymap for fast comment it's with : gcc | More than 1 line it's with : gc{lines} k or j, example gc10j <- comments 10 lines down
 -- also, with vim surround we can surround a word with the keymap : ysw"", to undo this, we can do: ds"", or if i want to change "" to '', we can do cs""''
 -- gb coment a block line
